11b9a90e96db559848b8a9f75fbd1c5492236754
dozieaji <dozie.aji@gmail.com>
2020-08-02 04:43:43 +0100

    Sample Terraform script added

---
+++ sampleProject/.sh
@@ -0,0 +1,8 @@
+export TF_VAR_tenancy_ocid=ocid1.tenancy.oc1..aaaaaaaap2qv72rfpapzs26rl47kxzzgqlyccnyk366vehba22m24aamiwwa
+export TF_VAR_user_ocid=ocid1.user.oc1..aaaaaaaax6wj2xuusv4bllztnij5otywcgws32d5hmug3n6yyhzeu6dv3nfa
+export TF_VAR_fingerprint=59:b6:9b:45:4a:18:f6:bc:ca:6c:3c:44:7e:fb:76:d2
+export TF_VAR_private_key_path=oci_api_key.pem
+### Region
+export TF_VAR_region=us-ashburn-1
+### Compartment
+export TF_VAR_compartment_ocid=ocid1.compartment.oc1..aaaaaaaaxx6d3usdu3x7arl6rpxpckjlrrgopdybkhwltvxtzdoh5vdtc2yq
---
+++ sampleProject/vcn.tf
@@ -0,0 +1,33 @@
+variable "tenancy_ocid" {
+}
+
+variable "user_ocid" {
+}
+
+variable "fingerprint" {
+}
+
+variable "private_key_path" {
+}
+
+variable "compartment_ocid" {
+}
+
+variable "region" {
+}
+
+provider "oci" {
+  version          = ">= 3.0.0"
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+  region           = var.region
+}
+resource "oci_core_virtual_network" "simple-vcn" {
+  cidr_block     = "10.0.0.0/16"
+  dns_label      = "vcn1"
+  compartment_id = "${var.compartment_ocid}"
+  display_name   = "simple-vcn"
+}
+
